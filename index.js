7beb5b628be1669bde8fd2e6ca072f40e05e80d8
Eduardo Pastor <edo.pstr@gmail.com>
2022-02-14 21:08:06 -0600

    something

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const config = {
     host:"localhost",
     database:"northwind",
     password:"556212177",
-    port:54320
+    port:5432
 }
 
 async function poolData (){
     const pool = new Pool(config);
-    const employees = await pool.query("select employee_id as employeeID, first_name as employeeName, last_name as employeeLastName from employees");
-    console.log(JSON.stringify(employees.rows[0],null,"  "));
-    
+    const employees = await pool.query(`
+    SELECT
+     employee_id as Id,
+     CONCAT(title_of_courtesy,' ',first_name,' ',last_name) as Employee,
+     home_phone as Phone,
+     photo as Photo
+    from employees`);
+    const getEmployees = JSON.stringify(employees.rows,null," ");
+    return getEmployees
+    console.log(getEmployees);   
 }
 
-poolData();
